d3433b2e67ee6293cf39ccb8c5fab55e8aa61be8
Astryxion <s.pergolotti@my.ccsu.edu>
2026-04-13 03:49:23 -0400

    Packed src

--- src/main/java/com/github/alexthe666/alexsmobs/item/CustomTabBehavior.java
+++
@@ -1,7 +0,0 @@
-package com.github.alexthe666.alexsmobs.item;
-
-import net.minecraft.world.item.CreativeModeTab;
-
-public interface CustomTabBehavior {
-    void fillItemCategory(CreativeModeTab.Output contents);
-}
